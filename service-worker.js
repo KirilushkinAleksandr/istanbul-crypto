6030655a192f966cea826eb99406c08007ca3252
Aleksandr Kirilushkin <Aleksandr_Kirilushkin@epam.com>
2022-12-27 13:24:01 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/istanbul-crypto/precache-manifest.bd364fa560a2a5a8c601de109ad74200.js"
+  "/istanbul-crypto/precache-manifest.4f96467ef2997607b1432eb70b77c1b4.js"
 );
 
 workbox.clientsClaim();
