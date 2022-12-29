4c381f4073a6c6b25116dc7a9d809697315f53ca
NurAliia_B <nur_aliia_battalova@epam.com>
2022-12-29 10:42:01 +0300

    refactor table and form

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer lang="en" className="footer">
-      <p className="footer__item">&copy; Antalya 2022 </p>
+      <p className="footer__item">&copy; Istanbul 2022 </p>
       <p><a className="footer__item" href="tel:05323615498">05323615498</a></p>
       <p className="footer__item">bjbi@mail.ru</p>
     </footer>
