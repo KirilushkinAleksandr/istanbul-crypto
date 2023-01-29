dfc8f0cd3f3b538c1968bbbc4d82b477cf62b565
Aleksandr Kirilushkin <Aleksandr_Kirilushkin@epam.com>
2023-01-29 18:30:52 +0300

    Updates

---
+++ precache-manifest.ff9bc6422306947c75b32cc665fd3b79.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = [
+  {
+    "revision": "41698f7615dad3368549",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/css/main.13478cb7.chunk.css"
+  },
+  {
+    "revision": "41698f7615dad3368549",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/js/main.c23ab11b.chunk.js"
+  },
+  {
+    "revision": "ff65595087e9afb23d25",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/js/runtime~main.a92bb741.js"
+  },
+  {
+    "revision": "deddf90d9f99931bd1bc",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/js/2.bf658238.chunk.js"
+  },
+  {
+    "revision": "f357f7882a4bd5bcec5f",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/js/3.37352ce7.chunk.js"
+  },
+  {
+    "revision": "910197f5f10d3af5e75a3333fbb9bac9",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/RUB.910197f5.svg"
+  },
+  {
+    "revision": "ffbac567e16ab8c1ed5199b99b605bac",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/USD.ffbac567.svg"
+  },
+  {
+    "revision": "08a3046924ff0018e009cd4050865cd2",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/EUR.08a30469.svg"
+  },
+  {
+    "revision": "717080ccce5ec36ecad0fb9b3f6f0adb",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/logo.717080cc.svg"
+  },
+  {
+    "revision": "c3390d80450759475e691a7fa1b8be35",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/GBP.c3390d80.svg"
+  },
+  {
+    "revision": "d810ecd05699e8fc26aaf91f4b4576e0",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/payporter.d810ecd0.jpg"
+  },
+  {
+    "revision": "334ba6ad1fe38dc87feca65a48ef21ee",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/instagram.334ba6ad.svg"
+  },
+  {
+    "revision": "4709b06d5acaa18650b73582773e8a32",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/whatsapp.4709b06d.svg"
+  },
+  {
+    "revision": "4c49f63bbfa88924a78ae6d878b107f0",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/telegram.4c49f63b.svg"
+  },
+  {
+    "revision": "dceac5562f22211749b62f2bb8555351",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/about-1.dceac556.jpg"
+  },
+  {
+    "revision": "793bdff956767e2fa1d2268125babd7f",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/about-2.793bdff9.jpg"
+  },
+  {
+    "revision": "81f7dc819f03080d6d25cec598314292",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/aero-1.81f7dc81.jpg"
+  },
+  {
+    "revision": "f683c1a8c1a4005c50625066a8b9ed8d",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/payporter-mobile.f683c1a8.jpg"
+  },
+  {
+    "revision": "28e02e487e967bb70e61252eea7109af",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/arrows.28e02e48.svg"
+  },
+  {
+    "revision": "b04ac1e231e9b9f0ad52d81948a6de5a",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/flag-TR.b04ac1e2.png"
+  },
+  {
+    "revision": "0385cea813578b0dedce72f90d89bc25",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/AUD.0385cea8.svg"
+  },
+  {
+    "revision": "7370c94e66842207890e1b4d003c9d4f",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/CAD.7370c94e.svg"
+  },
+  {
+    "revision": "e55b62da2da965564f861336497dcfff",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/CHF.e55b62da.svg"
+  },
+  {
+    "revision": "6b9d54d8d0ae228cb91d430751b91eb8",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/DKK.6b9d54d8.svg"
+  },
+  {
+    "revision": "2ced1009842bae1308725b198bc374d1",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/JPI.2ced1009.svg"
+  },
+  {
+    "revision": "16c1526b7ac5612ace33c806fc8c66d7",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/KWD.16c1526b.svg"
+  },
+  {
+    "revision": "822f92ca6fa419a17c607eb0465272c3",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/NOK.822f92ca.svg"
+  },
+  {
+    "revision": "d4cf61f0a7905e31f529d85d67e120a9",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/SAR.d4cf61f0.svg"
+  },
+  {
+    "revision": "206221e8558f100c61d84fda98407a69",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/SEK.206221e8.svg"
+  },
+  {
+    "revision": "0f0460f70ee27ff8d85f22224cf094e5",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/UAH.0f0460f7.svg"
+  },
+  {
+    "revision": "e9f02ea277831a0ea955507ed7b86349",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/minus.e9f02ea2.svg"
+  },
+  {
+    "revision": "86be70089450e9ba1d5b8d2b49f352a0",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/SF-Mono-Semibold.86be7008.otf"
+  },
+  {
+    "revision": "d6cd444515f72d0508bf106dbe42bbae",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/SF-Mono-Medium.d6cd4445.otf"
+  },
+  {
+    "revision": "096048104d0ca07dddb7aab3b39b495a",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/SF-Mono-Bold.09604810.otf"
+  },
+  {
+    "revision": "e981e8dcf70e0413e00f8ad42e24bc9c",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/plus.e981e8dc.svg"
+  },
+  {
+    "revision": "8e9701d8efc7df15706b06712b1c6de1",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/SF-Mono-Regular.8e9701d8.otf"
+  },
+  {
+    "revision": "0e9c24e2e9015ec4284addc27012a91d",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/SF-Mono-Heavy.0e9c24e2.otf"
+  },
+  {
+    "revision": "e6567f397c55dba8523a4f9b7bf7174e",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/bg-lines.e6567f39.png"
+  },
+  {
+    "revision": "b6081171840e488e95eb86518b087333",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/light-faq.b6081171.png"
+  },
+  {
+    "revision": "6607388bde0232ec9c94fc059b3788aa",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/light-about.6607388b.png"
+  },
+  {
+    "revision": "561766e87833546fb5e3966a250aecf7",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/light-address.561766e8.png"
+  },
+  {
+    "revision": "3255349c9013bf758ed9eb3df4a3d131",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/graph.3255349c.png"
+  },
+  {
+    "revision": "5cbbe2e802d5214644c7652ee80b999d",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/light-contact.5cbbe2e8.png"
+  },
+  {
+    "revision": "c25570c5f01f4e26781f11b07b84d371",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/bg.c25570c5.png"
+  },
+  {
+    "revision": "94ba7549321db81dbf310529441a405f",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/static/media/bitc.94ba7549.jpg"
+  },
+  {
+    "revision": "9ca8734d5ba96b0de1a8d5fa0fd872ce",
+    "url": "kirilushkinaleksandr.github.io/istanbul-crypto/index.html"
+  }
+];
