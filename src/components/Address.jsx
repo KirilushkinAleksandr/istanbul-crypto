84680dd44d23d1e97c9b933c9dcb29688f013613
Aleksandr Kirilushkin <Aleksandr_Kirilushkin@epam.com>
2023-02-01 11:57:40 +0300

    feat: add actual address on google maps

--- src/components/Address.jsx
+++ src/components/Address.jsx
@@ -12,7 +12,7 @@ function Address() {
       </div>
       <div className="content__block text__center content-transform flex flex-between">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d188.21273927701253!2d28.909044963804995!3d40.99453359844857!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cabb9f8113cfe1%3A0x30befdffda5b1199!2s37.%20Sokak!5e0!3m2!1sru!2str!4v1674328305962!5m2!1sru!2str"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d288.7418070791247!2d28.908683471198504!3d40.99452387575407!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cabb3b0ba454cf%3A0x3f8b94263bab28b4!2sBJBi!5e0!3m2!1sru!2str!4v1674999925709!5m2!1sru!2str"
           width="400"
           height="300"
           loading="lazy"
