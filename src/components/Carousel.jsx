f0c8ece4c672bfd6c6ed1bde7533aa72a8a78752
Aleksandr Kirilushkin <Aleksandr_Kirilushkin@epam.com>
2023-02-21 13:47:54 +0300

    feat: add mock photos to carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
-import office1 from "../images/center.jpeg";
-import office2 from "../images/office02.jpg";
-import office3 from "../images/office03.jpg";
-// import office4 from "../images/left.jpg";
-import office5 from "../images/right.jpeg";
+import office1 from "../images/office/office01.jpg";
+import office2 from "../images/office/office02.jpg";
+import office3 from "../images/office/office03.jpg";
+// import office4 from "../images/office/wrong-office04.jpg";
+import office5 from "../images/office/office05.jpg";
+import office6 from "../images/office/office06.jpg";
+import office7 from "../images/office/office07.jpg";
+import office8 from "../images/office/office08.jpg";
+import office9 from "../images/office/office09.jpg";
+import office10 from "../images/office/office10.jpg";
+import office11 from "../images/office/office11.jpg";
 import prev from "../images/prev.png";
 import next from "../images/next.png";
 
@@ -16,7 +22,18 @@ const CarouselItem = ({ imgSRC }) => {
   )
 };
 
-const imgArray = [office1, office2, office3, office5];
+const imgArray = [
+  office1,
+  office2,
+  office3,
+  office5,
+  office6,
+  office7,
+  office8,
+  office9,
+  office10,
+  office11,
+];
 
 function Carousel() {
   const { t } = useTranslation();
